11f8ceb06146156f56f80b0c9b7e9313bc19fbe6
Amanda Kedaigle <daigle7209@gmail.com>
2021-01-26 15:20:17 -0500

    Update Figures_for_pval - checks for order of rows

--- RandomForest/Figures_for_pval.R
+++ RandomForest/Figures_for_pval.R
@@ -22,18 +22,16 @@ for (timept in c("1m", "3m","6m")) {
   tcount = 1
   
   for (pathway in levels(factor(bg_pathways))) {
-  
-    bgSub = bg_files[bg_timepts==timept & bg_pathways==pathway]
-    if (length(bgSub) < 5000) print(paste0(timept," ",pathway," ", length(bgSub), "< 5,000!"))
-    bgSub = bgSub[1:5000]
-    length = length(bgSub) * nr
-    bgdist = data.frame(Var1 = rep(NA, length),Var2 = rep(NA, length),variable = rep(NA, length),value = rep(NA, length),
-                        FullFreq = rep(NA, length),diff = rep(NA, length))
+
     t_full = readRDS(full_files[full_timepts==timept])
   
     rmfile = rm_files[rm_timepts==timept & rm_pathways==pathway]
     if (length(rmfile)!=1) print(paste0(timept," ",pathway," ", length(rmfile), "Not one RM'd file!"))
     t1 = readRDS(rmfile)
+    t1$tog = paste0(t1$Var1, t1$Var2)
+    t_full$tog = paste0(t_full$Var1, t_full$Var2)
+    t_full = t_full[match(t1$tog, t_full$tog),]
+    if (sum(t_full$Var1!=t1$Var1)>0 | sum(t_full$Var2!=t1$Var2)>0) {print(paste0(pathway," ERROR with t_full and t1 not matching"))}
     t1$timept = timept
     t1$pathway = pathway
   
@@ -42,11 +40,20 @@ for (timept in c("1m", "3m","6m")) {
     t1$numLarger = 0
     t1$numLargerEq = 0
     
-    for (j in 2:length(bgSub)) {
+    bgSub = bg_files[bg_timepts==timept & bg_pathways==pathway]
+    if (length(bgSub) < 5750) print(paste0(timept," ",pathway," ", length(bgSub), "< 5,750!"))
+    bgSub = bgSub[1:5000]
+    length = length(bgSub) * nr
+    bgdist = data.frame(Var1 = rep(NA, length),Var2 = rep(NA, length),variable = rep(NA, length),value = rep(NA, length),
+                        FullFreq = rep(NA, length),diff = rep(NA, length))
+    
+    for (j in 1:length(bgSub)) {
       tBG = readRDS(bgSub[j])
+      tBG$tog = paste0(tBG$Var1, tBG$Var2)
+      tBG = tBG[match(t1$tog, tBG$tog),]
+      if (sum(tBG$Var1!=t1$Var1)>0 | sum(tBG$Var2!=t1$Var2)>0) {print("ERROR with tBG and t1 not matching")}
       tBG$FullFreq = t_full$value
       tBG$diff = tBG$value - tBG$FullFreq
-      if (sum(tBG$Var1!=t1$Var1)>0 | sum(tBG$Var2!=t1$Var2)>0) {print("ERROR with tBG and t1 not matching")}
       t1[tBG$diff > t1$diff,"numLarger"] = t1$numLarger[tBG$diff > t1$diff] + 1
       t1[tBG$diff >= t1$diff,"numLargerEq"] = t1$numLargerEq[tBG$diff >= t1$diff] + 1
     }
@@ -55,17 +62,15 @@ for (timept in c("1m", "3m","6m")) {
     t1[,1] = as.character(t1[,1])
     t1[,2] = as.character(t1[,2])
     t1[,3] = as.character(t1[,3])
+    t1$tog = NULL
   
     t[tcount:(tcount+(nr-1)),] = t1
     tcount = tcount+nr
   }
   t$padj = p.adjust(t$p, method="BY")
-  #saveRDS(t, paste0("t_", timept, "-5k.rds"))
-  #t_whole[wholecount:(wholecount+nrow(t)-1)] = t
-  #wholecount = wholecount+nrow(t)
   t_whole = rbind(t_whole, t)
 }
-saveRDS(t_whole, "t_whole-5.5k.rds")
+saveRDS(t_whole, "t_whole-5k.rds")
 
 ggplot(t_whole, aes(pathway, -1*log(padj))) +
   geom_point(aes(color= Var1)) +
@@ -81,16 +86,16 @@ ggplot(t_whole[t_whole$Var2 %in% c("aRG", "PN") & t_whole$timept=="3m",], aes(pa
   theme_minimal() +
   theme(axis.text.x = element_text(angle=90, hjust=1)) +
   facet_grid(Var2~.) +
-  labs(x="", y="Human Cell Type", main="Cells changing to being classified as aRG")
+  labs(x="", y="Human Cell Type", main="Cells changing to being classified as aRG", fill="FDR")
 ggsave("CellsChangingToaRGandPN-3m-5kruns.pdf", units="in", width=10, height=10) 
 
-pdf(paste0("hypoxia_in_3m-5kruns.pdf"), width=7.3, height=3.5, useDingbats=F)
-ggplot(t_whole[t_whole$pathway=="HALLMARK_HYPOXIA" & t_whole$timept=="3m",], aes(Var1, Var2)) + 
+pdf(paste0("glycolysis_in_6m-5kruns.pdf"), width=7.3, height=3.5, useDingbats=F)
+ggplot(t_whole[t_whole$pathway=="HALLMARK_GLYCOLYSIS" & t_whole$timept=="6m",], aes(Var1, Var2)) + 
   geom_point(aes(size = padj, fill = diff, color=padj<0.05), pch=21, stroke=0.7) + theme_bw() +
   xlab("Human Cell Types") + 
   ylab("Organoid Cell Types") + 
   scale_color_manual(values=c("gray50","black"))+
-  scale_size_continuous(name="Adjusted p value", range = c(6,2)) +
+  scale_size_continuous(name="FDR", range = c(6,2)) +
   scale_fill_gradient2(low="cyan4", mid="white", high="brown4", name="Change in Fraction of Human Cells") + 
   theme(axis.text.x=element_text(angle=45, hjust=0.9))
 dev.off()
