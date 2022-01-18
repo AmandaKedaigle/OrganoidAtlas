095071457dc699db93c528960968202b90e2ffb3
Amanda Kedaigle <daigle7209@gmail.com>
2022-01-18 14:31:40 -0500

    Add RRHO off-diagonal

--- RRHO.R
+++ RRHO.R
@@ -58,3 +58,51 @@ ggplot(ps, aes(OrgType, HumanType, fill=normavg)) +
 ggsave("RRHO2-1m/normalizedAvg-uu-pval-allComparisons.pdf", width=5, height=6.1)
 
 saveRDS(ps, "RRHO2-1m/normalizedAvg-uu-pvals.rds")
+
+#Explore gene list differences...this is to look at the genes changed in one v the other
+uu = RRHO2_obj$genelist_uu #up in both - 1 is up in org, 2 is up in human, 3 is both
+
+write.table(uu[[3]], "RRHO2/PNvExN-upInBoth.txt", quote = F, row.names = F, col.names = F)
+write.table(uu[[2]][!uu[[2]] %in% uu[[3]]], "RRHO2/PNvExN-upOnlyInHuman.txt", quote = F, row.names = F, col.names = F)
+write.table(uu[[1]][!uu[[1]] %in% uu[[3]]], "RRHO2/PNvExN-upOnlyInOrg.txt", quote = F, row.names = F, col.names = F)
+
+#GO
+rrhoaRG <- as.character(read.table("RRHO2/aRGvvRG-upOnlyInOrg.txt")$V1)
+rrhoPN <- as.character(read.table("RRHO2/PNvExN-upOnlyInOrg.txt")$V1)
+
+genes = list(rrhoaRG, rrhoPN)
+names(genes) = c("aRGsig-OrganoidOnly", "PNsig-OrganoidOnly")
+
+ck = compareCluster(genes, fun="enrichGO", OrgDb = org.Hs.eg.db, keyType = "SYMBOL", ont="BP")
+ck2 = clusterProfiler::simplify(ck,cutoff=0.7, by="p.adjust", select_fun=min)
+ck2b <- ck2
+levels <- names(genes)[order(names(genes))]
+ck2b@compareClusterResult$Cluster <- factor(ck2b@compareClusterResult$Cluster, levels = levels)
+
+finalplot <- dotplot(ck2b, showCategory = 3) + theme(axis.text.x=element_text(angle=45, hjust=1)) + scale_colour_gradient2(low = "#c2a5cf", mid = "#984ea3", high = "#ff7f00")
+
+pdf('enrichGO_PNandaRG_cat3.pdf', width = 8.5, height=4.5)
+print(finalplot)
+dev.off()
+
+#for KEGG, we need entrez ids
+genesEntrez = list()
+for (l in names(genes)) {
+  genesEntrez[[l]] = mapIds(org.Hs.eg.db,
+                            keys=genes[[l]],
+                            keytype="SYMBOL",
+                            column="ENTREZID",
+                            multiVals="first")
+}
+
+kk = compareCluster(genesEntrez, fun="enrichKEGG", organism= "hsa", pvalueCutoff = 1)
+kkb <- kk
+levels <- names(genes)[order(names(genes))]
+kkb@compareClusterResult$Cluster <- factor(kkb@compareClusterResult$Cluster, levels = levels)
+
+finalplot1 <- dotplot(kkb, showCategory=5) + theme(axis.text.x=element_text(angle=45, hjust=1)) + ggtitle("enrichKEGG Cell Types") + scale_colour_gradient2(low = "#c2a5cf", mid = "#984ea3", high = "#ff7f00")
+
+pdf('enrichKEGG_PNandaRG_cat5.pdf', width = 8.5, height=4.5)
+print(finalplot1)
+dev.off()
+
