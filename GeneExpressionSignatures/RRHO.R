56618f52505e135c31749a2994e70e930eea5e93
Amanda Kedaigle <daigle7209@gmail.com>
2022-03-16 11:33:34 -0400

    Correct gene signature methods

--- GeneExpressionSignatures/RRHO.R
+++ GeneExpressionSignatures/RRHO.R
@@ -43,46 +43,46 @@ for (of in list.files(path = "DESeq-organoids-1mo/")) {
 uu = RRHO2_obj$genelist_uu #up in both - 1 is up in org, 2 is up in human, 3 is both
 
 write.table(uu[[3]], "RRHO2/PNvExN-upInBoth.txt", quote = F, row.names = F, col.names = F)
-write.table(uu[[2]][!uu[[2]] %in% uu[[3]]], "RRHO2/PNvExN-upOnlyInHuman.txt", quote = F, row.names = F, col.names = F)
-write.table(uu[[1]][!uu[[1]] %in% uu[[3]]], "RRHO2/PNvExN-upOnlyInOrg.txt", quote = F, row.names = F, col.names = F)
+write.table(uu[[2]][!uu[[2]] %in% uu[[3]]], "RRHO2/OutofDiagBoth/PNvExN-upOnlyInHuman.txt", quote = F, row.names = F, col.names = F)
+write.table(uu[[1]][!uu[[1]] %in% uu[[3]]], "RRHO2/OutofDiagBoth/PNvExN-upOnlyInOrg.txt", quote = F, row.names = F, col.names = F)
 
-#GO
-rrhoaRG <- as.character(read.table("RRHO2/aRGvvRG-upOnlyInOrg.txt")$V1)
-rrhoPN <- as.character(read.table("RRHO2/PNvExN-upOnlyInOrg.txt")$V1)
-
-genes = list(rrhoaRG, rrhoPN)
-names(genes) = c("aRGsig-OrganoidOnly", "PNsig-OrganoidOnly")
-
-ck = compareCluster(genes, fun="enrichGO", OrgDb = org.Hs.eg.db, keyType = "SYMBOL", ont="BP")
-ck2 = clusterProfiler::simplify(ck,cutoff=0.7, by="p.adjust", select_fun=min)
-ck2b <- ck2
-levels <- names(genes)[order(names(genes))]
-ck2b@compareClusterResult$Cluster <- factor(ck2b@compareClusterResult$Cluster, levels = levels)
-
-finalplot <- dotplot(ck2b, showCategory = 3) + theme(axis.text.x=element_text(angle=45, hjust=1)) + scale_colour_gradient2(low = "#c2a5cf", mid = "#984ea3", high = "#ff7f00")
-
-pdf('enrichGO_PNandaRG_cat3.pdf', width = 8.5, height=4.5)
-print(finalplot)
-dev.off()
-
-#for KEGG, we need entrez ids
-genesEntrez = list()
-for (l in names(genes)) {
-  genesEntrez[[l]] = mapIds(org.Hs.eg.db,
-                            keys=genes[[l]],
-                            keytype="SYMBOL",
-                            column="ENTREZID",
-                            multiVals="first")
+#Look at overlap with gene lists
+d1 = "~/Documents/Arlotta/Organoids/Ana_Amanda/Paper/Signatures_071321/TolookforRRHO/Metabolic_pathways/"
+d2 = "~/Documents/Arlotta/Organoids/Ana_Amanda/Paper/Signatures_071321/RRHO2_3mo_1/OutofDiagBoth/"
+fl1 = list.files(path=d1, pattern=".txt")
+fl2 = list.files(path=d2, pattern=".txt")
+overlaps = data.frame() #save how big the overlap is for each comparison
+for (newfile in fl1) {
+  newfilepath = file.path(d1, newfile)
+  for (upfile in fl2) {
+    upfilepath = file.path(d2, upfile)
+    newList = as.character(read.table(newfilepath)[,1])
+    upList = as.character(read.table(upfilepath)[,1])
+    overlap = intersect(newList, upList)
+    lapply(overlap, write, paste0(substr(newfile, 1, nchar(newfile)-4),
+                                  substr(upfile, 1, nchar(upfile)-4),
+                                  "_overlap.txt"), append=TRUE)
+    overlaps[newfile,upfile] = length(overlap)
+  }
 }
-
-kk = compareCluster(genesEntrez, fun="enrichKEGG", organism= "hsa", pvalueCutoff = 1)
-kkb <- kk
-levels <- names(genes)[order(names(genes))]
-kkb@compareClusterResult$Cluster <- factor(kkb@compareClusterResult$Cluster, levels = levels)
-
-finalplot1 <- dotplot(kkb, showCategory=5) + theme(axis.text.x=element_text(angle=45, hjust=1)) + ggtitle("enrichKEGG Cell Types") + scale_colour_gradient2(low = "#c2a5cf", mid = "#984ea3", high = "#ff7f00")
-
-pdf('enrichKEGG_PNandaRG_cat5.pdf', width = 8.5, height=4.5)
-print(finalplot1)
+#hypergeometric test
+ps = data.frame()
+for (r in rownames(overlaps)) {
+  for (c in colnames(overlaps)) {
+    universe = 33694 #This is the number of genes in one of our seurat objects
+    rowsize = length(as.character(read.table(file.path(d1, r))[,1]))
+    colsize = length(as.character(read.table(file.path(d2, c))[,1]))
+    numoverlap = overlaps[r,c]
+    p = phyper(numoverlap-1, rowsize, universe-rowsize, colsize, lower.tail = F)
+    ps[r,c] = p
+  }
+}
+numTests = sum(1:(nrow(overlaps)-1))
+padjs = apply(ps, c(1,2), function (x) min(x*numTests, 1))
+padjs = apply(padjs, c(1,2), function (x) max(x, 10^-100))
+logps = as.matrix(-1* log10(padjs))
+library(corrplot)
+tiff('Both_3mo_met_Poliu.tiff', width = 3000, height= 8000, res = 300)
+plot.new()
+corrplot(logps, is.corr=F, type = "lower", diag = F, tl.col = "black", cl.length=2)
 dev.off()
-
